1d63aefe6a6f6778462fa6a0da9df4a44289ddb4
Mariano Wadel <marianowadel@gmail.com>
2015-02-26 19:46:33 -0300

    Added config and removed unused folders

---
+++ streaming/config.js
@@ -0,0 +1,19 @@
+/**
+ * Twitter App Config.
+ * Romain Huet
+ * @romainhuet
+ */
+
+// Create your Twitter application on https://apps.twitter.com/.
+// Copy this file to config.js and add your private Twitter API credentials.
+
+// The Streaming APIs demos require only the Read-Only permission.
+// Note: The "custom" demo needs access to the Custom Timelines API Beta,
+// but the streaming part of the demo works without it.
+
+module.exports = {
+  consumer_key: 'gSumxe0Scp7BY7fjQv9Dht0jf ',
+  consumer_secret: 'JtgOjCSVDiNYNL3IAC5SlQMK2JVVmBEKkJo1rQ2E6OQiKRmp5M',
+  access_token: '1100784985-sqPCouJMvCVXSUcE4299U6rWyavVEYZklmjYUda',
+  access_token_secret: 'C9zJeMFn9tHk7WTwL7NIAWcZCCzMhkiUTbEnT7hs8f2Ge'
+};
