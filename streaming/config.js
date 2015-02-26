61be1d387ba63f68dc097db42530d358bf3afd3b
Mariano Wadel <marianowadel@gmail.com>
2015-02-26 20:50:38 -0300

    Fixed typo in auth token

--- streaming/config.js
+++ streaming/config.js
@@ -12,7 +12,7 @@
 // but the streaming part of the demo works without it.
 
 module.exports = {
-  consumer_key: 'gSumxe0Scp7BY7fjQv9Dht0jf ',
+  consumer_key: 'gSumxe0Scp7BY7fjQv9Dht0jf',
   consumer_secret: 'JtgOjCSVDiNYNL3IAC5SlQMK2JVVmBEKkJo1rQ2E6OQiKRmp5M',
   access_token: '1100784985-sqPCouJMvCVXSUcE4299U6rWyavVEYZklmjYUda',
   access_token_secret: 'C9zJeMFn9tHk7WTwL7NIAWcZCCzMhkiUTbEnT7hs8f2Ge'
